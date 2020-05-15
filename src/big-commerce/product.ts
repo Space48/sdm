d96b668aadf9f84166dd2d617c3be786079296df
Josh Di Fabio <joshdifabio@gmail.com>
2020-05-15 11:35:39 +0100

    Add bc:product:get

--- src/big-commerce/product.ts
+++ src/big-commerce/product.ts
@@ -1,6 +1,6 @@
 import { sink } from "./common";
 import BigCommerce from "./client";
-import { source } from "@space48/json-pipe";
+import { source, concurrentMap } from "@space48/json-pipe";
 
 export const actions = {
     create: (bigCommerce: BigCommerce) => sink(data => bigCommerce.post('v3/catalog/products', data)),
@@ -8,6 +8,9 @@ export const actions = {
     delete: (bigCommerce: BigCommerce) => sink(productId => bigCommerce.delete(`v3/catalog/products/${productId}`)),
 
     list: (bigCommerce: BigCommerce) => source(bigCommerce.list('v3/catalog/products', {include})),
+
+    get: (bigCommerce: BigCommerce) =>
+        concurrentMap(50, productId => bigCommerce.get(`v3/catalog/products/${productId}`, {include}).then(({data}) => data)),
 };
 
 const include = 'variants,images,custom_fields,bulk_pricing_rules,primary_image,modifiers,options,videos';
