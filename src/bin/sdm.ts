ba13c89353b781a1b546dfafbcf8942cc9e70005
Josh Di Fabio <joshdifabio@gmail.com>
2021-04-01 12:15:59 +0100

    Framework: Rename Stateful to State

--- src/bin/sdm.ts
+++ src/bin/sdm.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { Command, ConnectorDefinition, EndpointError, FullyQualifiedMessageHeader, Path, ScopeRef, Stateful } from "../framework";
+import { Command, ConnectorDefinition, EndpointError, FullyQualifiedMessageHeader, Path, ScopeRef, State } from "../framework";
 import * as readline from "readline";
 import { map, pipe, takeWhile, tap, readJsonLinesFrom, writeJsonLinesTo, writeTo } from "@space48/json-pipe";
 import chalk from "chalk";
@@ -81,7 +81,7 @@ async function runNonInteractiveMode() {
   } else {
     await pipe(
       readJsonLinesFrom(process.stdin),
-      Stateful.map((input): Command => ({ ...command, input })),
+      State.map((input): Command => ({ ...command, input })),
       commands => scope.execute(commands),
       writeJsonLinesTo(process.stdout),
     );
